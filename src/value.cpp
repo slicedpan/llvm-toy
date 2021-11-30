de9a454537f93c547e37214bed2e84269adc7ad4
Owen Mooney <omooney@tcd.ie>
2021-11-30 21:02:00 +0000

    added unit tests

--- src/value.cpp
+++ src/value.cpp
@@ -1,139 +1,139 @@
-#include "value.hpp"
-#include "memory_allocation.hpp"
-#include <cstring>
-#include <bitset>
+// #include "value.hpp"
+// #include "memory_allocation.hpp"
+// #include <cstring>
+// #include <bitset>
 
-namespace LLVMToy {
-  string Value::hexdump() const {
-    stringstream ss;
-    ss << std::hex << type << pad[0] << pad[1] << pad[2] << pad[3] << pad[4] << pad[5] << pad[6] << int_value;
-    return ss.str();
-  }
+// namespace LLVMToy {
+//   string Value::hexdump() const {
+//     stringstream ss;
+//     ss << std::hex << type << pad[0] << pad[1] << pad[2] << pad[3] << pad[4] << pad[5] << pad[6] << int_value;
+//     return ss.str();
+//   }
 
-  string Value::bindump() const {
-    stringstream ss;
-    ss << std::bitset<8>{type} << std::bitset<8>{pad[0]} << std::bitset<8>{pad[1]} << std::bitset<8>{pad[2]}
-      << std::bitset<8>{pad[3]} << std::bitset<8>{pad[4]} << std::bitset<8>{pad[5]} << std::bitset<8>{pad[6]}
-      << std::bitset<64>{int_value};
-    return ss.str();
-  }
+//   string Value::bindump() const {
+//     stringstream ss;
+//     ss << std::bitset<8>{type} << std::bitset<8>{pad[0]} << std::bitset<8>{pad[1]} << std::bitset<8>{pad[2]}
+//       << std::bitset<8>{pad[3]} << std::bitset<8>{pad[4]} << std::bitset<8>{pad[5]} << std::bitset<8>{pad[6]}
+//       << std::bitset<64>{int_value};
+//     return ss.str();
+//   }
 
-  string Value::to_string() const {
-    stringstream ss;
-    switch (type) {
-      case ValueType::Integer:
-        ss << int_value << "i";
-        break;
-      case ValueType::Function:
-        ss << "function:" << function;
-        break;
-      case ValueType::NativeFunction:
-        ss << "native_function:" << native_ptr;
-        break;
-      case ValueType::String:
-        ss << "\"" << string_value << "\"";
-        break;
-      case ValueType::FloatingPoint:
-        ss << fp_value << "f";
-        break;
-      case ValueType::Nil:
-        ss << "nil";
-        break;
-      case ValueType::Undefined:
-        ss << "undefined";
-        break;
-      case ValueType::Boolean:
-        ss << (bool_value ? "true" : "false");
-        break;
-      case ValueType::NativePointer:
-        ss << "ptr: " << native_ptr;
-        break;
-    }
-    return ss.str();
-  }
+//   string Value::to_string() const {
+//     stringstream ss;
+//     switch (type) {
+//       case ValueType::Integer:
+//         ss << int_value << "i";
+//         break;
+//       case ValueType::Function:
+//         ss << "function:" << function;
+//         break;
+//       case ValueType::NativeFunction:
+//         ss << "native_function:" << native_ptr;
+//         break;
+//       case ValueType::String:
+//         ss << "\"" << string_value << "\"";
+//         break;
+//       case ValueType::FloatingPoint:
+//         ss << fp_value << "f";
+//         break;
+//       case ValueType::Nil:
+//         ss << "nil";
+//         break;
+//       case ValueType::Undefined:
+//         ss << "undefined";
+//         break;
+//       case ValueType::Boolean:
+//         ss << (bool_value ? "true" : "false");
+//         break;
+//       case ValueType::NativePointer:
+//         ss << "ptr: " << native_ptr;
+//         break;
+//     }
+//     return ss.str();
+//   }
 
-  void Value::free_self() {
-    switch (type) {
-      case ValueType::String:
-        deallocate(string_value);
-        break;
-      case ValueType::Function:
-        delete function;
-        break;
-    }
-  }
+//   void Value::free_self() {
+//     switch (type) {
+//       case ValueType::String:
+//         deallocate(string_value);
+//         break;
+//       case ValueType::Function:
+//         delete function;
+//         break;
+//     }
+//   }
 
-  bool Value::is_truthy() {
-    switch(type) {
-      case ValueType::String:
-      case ValueType::Function:
-      case ValueType::NativeFunction:
-      case ValueType::Integer:
-      case ValueType::FloatingPoint:
-        return true;
-        break;
-      case ValueType::Boolean:
-        return bool_value;
-        break;
-    }
-    // nil or undefined
-    return false;
-  }
+//   bool Value::is_truthy() {
+//     switch(type) {
+//       case ValueType::String:
+//       case ValueType::Function:
+//       case ValueType::NativeFunction:
+//       case ValueType::Integer:
+//       case ValueType::FloatingPoint:
+//         return true;
+//         break;
+//       case ValueType::Boolean:
+//         return bool_value;
+//         break;
+//     }
+//     // nil or undefined
+//     return false;
+//   }
 
-  Value Value::make_native_ptr(void* ptr) {
-    Value val{ ValueType::NativePointer };
-    val.native_ptr = ptr;
-    return val;
-  }
+//   Value Value::make_native_ptr(void* ptr) {
+//     Value val{ ValueType::NativePointer };
+//     val.native_ptr = ptr;
+//     return val;
+//   }
 
-  Value Value::make_string(string src) {
-    Value val;
-    size_t memsize = (src.length() + 1) * sizeof(char);
-    val.type = ValueType::String;
-    val.string_value = (char*)allocate(memsize);
-    memcpy(val.string_value, src.c_str(), memsize);
-    return val;
-  }
+//   Value Value::make_string(string src) {
+//     Value val;
+//     size_t memsize = (src.length() + 1) * sizeof(char);
+//     val.type = ValueType::String;
+//     val.string_value = (char*)allocate(memsize);
+//     memcpy(val.string_value, src.c_str(), memsize);
+//     return val;
+//   }
 
-  Value Value::make_float(double src) {
-    Value val;
-    val.type = ValueType::FloatingPoint;
-    val.fp_value = src;
-    return val;
-  }
+//   Value Value::make_float(double src) {
+//     Value val;
+//     val.type = ValueType::FloatingPoint;
+//     val.fp_value = src;
+//     return val;
+//   }
 
-  Value Value::make_int(int src) {
-    Value val;
-    val.type = ValueType::Integer;
-    val.int_value = src;
-    return val;
-  }
+//   Value Value::make_int(int src) {
+//     Value val;
+//     val.type = ValueType::Integer;
+//     val.int_value = src;
+//     return val;
+//   }
 
-  Value Value::make_bool(bool src) {
-    Value val;
-    val.type = ValueType::Boolean;
-    val.bool_value = src;
-    return val;
-  }
+//   Value Value::make_bool(bool src) {
+//     Value val;
+//     val.type = ValueType::Boolean;
+//     val.bool_value = src;
+//     return val;
+//   }
 
-  Value Value::make_nil() {
-    Value val{ValueType::Nil};
-    return val;
-  }
+//   Value Value::make_nil() {
+//     Value val{ValueType::Nil};
+//     return val;
+//   }
 
-  Value Value::make_undefined() {
-    Value val{ValueType::Undefined};
-    return val;
-  }
+//   Value Value::make_undefined() {
+//     Value val{ValueType::Undefined};
+//     return val;
+//   }
 
-  Value Value::make_builtin_fn(const char* name) {
-    Value val;
-    if (strlen(name) > 7) {
-      return make_undefined();
-    } else {
-      val.type = ValueType::BuiltinFunction;
-      strcpy(val.short_str, name);
-    }
-    return val;
-  }
-}
+//   Value Value::make_builtin_fn(const char* name) {
+//     Value val;
+//     if (strlen(name) > 7) {
+//       return make_undefined();
+//     } else {
+//       val.type = ValueType::BuiltinFunction;
+//       strcpy(val.short_str, name);
+//     }
+//     return val;
+//   }
+// }
--- src/value.hpp
+++ src/value.hpp
@@ -3,10 +3,27 @@
 
 #include "namespace.hpp"
 #include <cstdint>
-
+#include <cstring>
+#include <bitset>
+#include "memory_allocation.hpp"
 
 namespace LLVMToy {
   class Function;
+
+  const uint64_t QNAN = 0b0111111111111100000000000000000000000000000000000000000000000000;
+  const uint64_t POINTER_BIT = 0b1000000000000000000000000000000000000000000000000000000000000000;
+  // including the pointer bit in the type mask means we can directly compare with the initial
+  // values like STR_POINTER below
+  const uint64_t POINTER_TYPE_MASK = 0b1000000000000000000000000000000000000000000000000000000000000111;
+  const uint64_t SHORT_STR_VALUE = 0b0000000000000001000000000000000000000000000000000000000000000000;
+  const uint64_t NIL_VALUE = 0b0111111111111100000000000000000000000000000000000000000000000001;
+  const uint64_t TRUE_VALUE = 0b0111111111111100000000000000000000000000000000000000000000000010;
+  const uint64_t FALSE_VALUE = 0b0111111111111100000000000000000000000000000000000000000000000011;
+  const uint64_t UNDEFINED_VALUE = 0b0111111111111100000000000000000000000000000000000000000000000100;
+  const uint64_t STR_POINTER = 0b1000000000000000000000000000000000000000000000000000000000000001;
+  const uint64_t GENERIC_POINTER = 0b1000000000000000000000000000000000000000000000000000000000000010;
+  const uint64_t FUNCTION_POINTER = 0b1000000000000000000000000000000000000000000000000000000000000011;
+  const uint64_t POINTER_HI_MASK = 0b0000000000000000111111111111111111111111111111111111111111111000;
 
   class ValueType {
     public:
@@ -19,36 +36,175 @@ namespace LLVMToy {
       static const uint8_t Nil = 6;
       static const uint8_t NativeFunction = 7;
       static const uint8_t BuiltinFunction = 8;
-      static const uint8_t NativePointer = 9;
+      static const uint8_t GenericPointer = 9;
+      static const uint8_t ShortString = 10;
   };
 
   struct Value {
-    uint8_t type;
-    uint8_t pad[7];
     union {
-      bool bool_value;
-      int64_t int_value;
-      double fp_value;
-      char* string_value;
-      Function* function;
-      void* native_ptr;
-      char short_str[8];
+      double float_value;
+      void* ptr_value;
+      char bytes[8];
+      uint64_t uint_value;
     };
-    string to_string() const;
-    string hexdump() const;
-    string bindump() const;
-    bool is_truthy();
 
-    static Value make_string(string src);
-    static Value make_int(int src);
-    static Value make_float(double src);
-    static Value make_bool(bool src);
-    static Value make_nil();
-    static Value make_undefined();
-    static Value make_builtin_fn(const char* name);
-    static Value make_native_ptr(void* ptr);
+    inline string bindump() {
+      stringstream ss;
+      ss << std::bitset<64>{uint_value};
+      return ss.str();
+    }
+
+    inline void* pointer_value() {
+       return (void*)(uint_value & POINTER_HI_MASK);
+    }
+
+    static inline Value make_string(string src) {
+      Value val;
+      if (src.length() <= 5) {
+        val.uint_value = SHORT_STR_VALUE | QNAN;
+        char* ptr = &val.bytes[0];
+        strcpy(ptr, src.c_str());   
+        return val;     
+      }
+      val.uint_value = STR_POINTER | QNAN;
+      char* char_ptr = (char*)allocate(sizeof(char) * (src.length() + 1));
+      strcpy(char_ptr, src.c_str());
+      char_ptr[src.length()] = '\0';
+      val.uint_value |= (uintptr_t)char_ptr & POINTER_HI_MASK;
+      return val; 
+    }
+
+    inline uint8_t get_type() {
+      if ((uint_value & QNAN) != QNAN) {
+        return ValueType::FloatingPoint;
+      }
+      switch (uint_value) {
+        case NIL_VALUE:
+          return ValueType::Nil;
+          break;
+        case TRUE_VALUE:
+        case FALSE_VALUE:
+          return ValueType::Boolean;
+          break;
+        case UNDEFINED_VALUE:
+          return ValueType::Undefined;
+          break;
+      }
+      if ((uint_value & POINTER_BIT)) {
+        switch (uint_value & POINTER_TYPE_MASK) {
+          case STR_POINTER:
+            return ValueType::String;
+            break;
+          case GENERIC_POINTER:
+            return ValueType::GenericPointer;
+            break;
+          case FUNCTION_POINTER:
+            return ValueType::Function;
+            break;
+        }
+      } else {
+        return ValueType::ShortString;        
+      }
+    }
+
+    static inline Value make_number(double src) {
+      Value val;
+      val.float_value = src;
+      return val;
+    }
+
+    static inline Value make_nil() {
+      Value val;
+      val.uint_value = NIL_VALUE;
+      return val;
+    }
+
+    static inline Value make_undefined() {
+      Value val;
+      val.uint_value = UNDEFINED_VALUE;
+      return val;
+    }
+
+    static inline Value make_true() {
+      Value val;
+      val.uint_value = TRUE_VALUE;
+      return val;
+    }
+
+    static inline Value make_false() {
+      Value val;
+      val.uint_value = FALSE_VALUE;
+      return val;
+    }
+
+    static inline Value make_bool(bool src) {
+      return src ? make_true() : make_false();
+    }
+
+    static inline Value make_untyped_ptr(void* ptr) {
+      Value val;
+      val.uint_value = QNAN | ((uintptr_t)ptr & POINTER_HI_MASK);
+      return val;
+    }
+
+    static inline Value make_generic_ptr(void* ptr) {
+      Value val = make_untyped_ptr(ptr);
+      val.uint_value |= GENERIC_POINTER;
+      return val;
+    }
+
+    static inline Value make_function_ptr(void* ptr) {
+      Value val = make_untyped_ptr(ptr);
+      val.uint_value |= FUNCTION_POINTER;
+      return val;
+    }
+
+    inline bool is_truthy() {
+      return (uint_value != FALSE_VALUE) && 
+      (uint_value != NIL_VALUE) && 
+      (uint_value != UNDEFINED_VALUE); 
+    }
+
+    inline string to_string() {
+      stringstream ss;
+      switch(get_type()) {
+        case ValueType::String:
+          ss << (char*)pointer_value();
+          break;
+        case ValueType::ShortString:
+          ss << &bytes[0];
+          break;
+        case ValueType::FloatingPoint:
+          ss << float_value;
+          break;
+        case ValueType::Nil:
+          ss << "nil";
+          break;
+        case ValueType::Undefined:
+          ss << "undefined";
+          break;
+        case ValueType::Boolean:
+          ss << (uint_value == TRUE_VALUE ? "true" : "false");
+          break;
+        case ValueType::GenericPointer:
+          ss << "generic_ptr: " << pointer_value();
+          break;
+        case ValueType::Function:
+          ss << "function: " << pointer_value();
+          break;
+      }
+      return ss.str();
+    }
+
+    // static Value make_int(int src);
+    // static Value make_float(double src);
+    // static Value make_bool(bool src);
+    // static Value make_nil();
+    // static Value make_undefined();
+    // static Value make_builtin_fn(const char* name);
+    // static Value make_native_ptr(void* ptr);
     
-    void free_self();
+    // void free_self();
 
 
   };
